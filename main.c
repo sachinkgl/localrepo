f578dad47a8d0a4f2051b573c208f13ec5339395
sachinkgl <sachinkgl03@gmail.com>
2026-03-23 13:25:02 +0530

    Add new feature

--- main.c
+++ main.c
@@ -3,4 +3,5 @@
 void main()
 {
     printf("This is new nested Repo if you do 'git init' \n Else if you just do 'git add .'it'll just treat it an a file.");
+    printf("This is a new feature\n");
 }
